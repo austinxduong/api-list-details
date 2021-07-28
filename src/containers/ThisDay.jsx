8c02c2e66f905ae8fb46e0ea1197193994abbaea
austinxduong <axduong2@gmail.com>
2021-07-28 10:31:53 -0700

    add event container

---
+++ src/containers/ThisDay.jsx
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import EventList from '../components/events/EventList';
+import { fetchEvents } from '../services/thisDayApi';
+
+export default class ThisDay extends Component {
+    state = {
+      events: [],
+      loading: true,
+    };
+
+    async componentDidMount() {
+      const events = await fetchEvents();
+      this.setState({ events, loading: false });
+    }
+
+    render() {
+      const { events, loading } = this.state;
+      if(loading) return <h1>En Route...</h1>;
+
+      return <EventList events={events} />;
+    }
+}
